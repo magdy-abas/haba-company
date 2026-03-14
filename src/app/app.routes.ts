83cfbae2f45cd7df9981d6fd7ce07558c082e2be
Magdy Abas <132621536+magdy-abas@users.noreply.github.com>
2026-03-14 20:56:58 +0200

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/pages/home/home.component').then(
+        (m) => m.HomeComponent,
+      ),
+  },
+
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/pages/about/about.component').then(
+        (m) => m.AboutComponent,
+      ),
+  },
+
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./components/pages/services/services.component').then(
+        (m) => m.ServicesComponent,
+      ),
+  },
+
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./components/pages/contact/contact.component').then(
+        (m) => m.ContactComponent,
+      ),
+  },
+
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import('./components/pages/blog/blog.component').then(
+        (m) => m.BlogComponent,
+      ),
+  },
+];
